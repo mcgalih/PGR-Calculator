6003b473fa09f7d5edc46d9d6793f9e56abb3dd3
mcgalih <mcgalihramdhan@gmail.com>
2023-12-19 16:51:55 +0700

    animate hamb icon

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,6 @@
 function toggleMobileMenu() {
     let hamb_menu = document.getElementById("hamb-menu");
+    let hamb_icon = document.getElementById("hamb-icon");
     hamb_menu.classList.toggle("show-navmenu");
+    hamb_icon.classList.toggle("open");
 }

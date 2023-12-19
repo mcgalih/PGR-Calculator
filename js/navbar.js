a1be6bbe4c166ef2b955e8d6c6d5aeb18dea17eb
mcgalih <mcgalihramdhan@gmail.com>
2023-12-19 17:19:49 +0700

    content display

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,45 @@
 function toggleMobileMenu() {
     let hamb_menu = document.getElementById("hamb-menu");
     let hamb_icon = document.getElementById("hamb-icon");
-    hamb_menu.classList.toggle("show-navmenu");
+    hamb_menu.classList.toggle("open");
     hamb_icon.classList.toggle("open");
+}
+
+function MenuConstruct() {
+    let hamb_menu = document.getElementById("hamb-menu");
+    let hamb_icon = document.getElementById("hamb-icon");
+    let menu_construct = document.getElementById("menu-construct");
+    let menu_weapon = document.getElementById("menu-weapon");
+    let menu_cub = document.getElementById("menu-cub");
+    hamb_menu.classList.toggle("open");
+    hamb_icon.classList.toggle("open");
+    menu_construct.style.display = "block";
+    menu_weapon.style.display = "none";
+    menu_cub.style.display = "none";
+}
+
+function MenuWeapon() {
+    let hamb_menu = document.getElementById("hamb-menu");
+    let hamb_icon = document.getElementById("hamb-icon");
+    let menu_construct = document.getElementById("menu-construct");
+    let menu_weapon = document.getElementById("menu-weapon");
+    let menu_cub = document.getElementById("menu-cub");
+    hamb_menu.classList.toggle("open");
+    hamb_icon.classList.toggle("open");
+    menu_construct.style.display = "none";
+    menu_weapon.style.display = "block";
+    menu_cub.style.display = "none";
+}
+
+function MenuCub() {
+    let hamb_menu = document.getElementById("hamb-menu");
+    let hamb_icon = document.getElementById("hamb-icon");
+    let menu_construct = document.getElementById("menu-construct");
+    let menu_weapon = document.getElementById("menu-weapon");
+    let menu_cub = document.getElementById("menu-cub");
+    hamb_menu.classList.toggle("open");
+    hamb_icon.classList.toggle("open");
+    menu_construct.style.display = "none";
+    menu_weapon.style.display = "none";
+    menu_cub.style.display = "block";
 }

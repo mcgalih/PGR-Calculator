faa68a5a3397a5453854d76a690f283014209011
mcgalih <mcgalihramdhan@gmail.com>
2023-12-20 17:25:33 +0700

    slider style

--- js/show_charlvl.js
+++ js/show_charlvl.js
@@ -2,5 +2,8 @@ const value = document.querySelector("#show_lvl_input");
 const lvl_input = document.querySelector("#lvl_input");
 value.textContent = lvl_input.value;
 lvl_input.addEventListener("input", (event) => {
-    value.textContent = event.target.value;
+    const tempSliderValue = event.target.value;
+    value.textContent = tempSliderValue;
+    const progress = (tempSliderValue / lvl_input.max) * 100;
+    lvl_input.style.background = `linear-gradient(to right, #940000 ${progress}%, #ccc ${progress}%)`;
 });

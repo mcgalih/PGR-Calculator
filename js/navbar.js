b8bb61ecc6b467418567283b2e899a4f1f93af37
mcgalih <mcgalihramdhan@gmail.com>
2023-12-23 08:22:25 +0700

    skill menu

--- js/navbar.js
+++ js/navbar.js
@@ -2,10 +2,10 @@ const hamb_menu = document.getElementById("hamb-menu");
 const hamb_icon = document.getElementById("hamb-icon");
 const nav_construct = document.getElementById("nav-construct");
 const menu_construct = document.getElementById("menu-construct");
-const nav_weapon = document.getElementById("nav-weapon");
-const menu_weapon = document.getElementById("menu-weapon");
 const nav_cub = document.getElementById("nav-cub");
 const menu_cub = document.getElementById("menu-cub");
+const nav_serum = document.getElementById("nav-serum");
+const menu_serum = document.getElementById("menu-serum");
 const nav_about = document.getElementById("nav-about");
 const menu_about = document.getElementById("menu-about");
 const btn_calculate = document.getElementById("btn-calculate");
@@ -21,17 +21,8 @@ hamb_icon.addEventListener("click", toggleNavbar)
 nav_construct.addEventListener("click", function () {
     toggleNavbar();
     menu_construct.style.display = "block";
-    menu_weapon.style.display = "none";
     menu_cub.style.display = "none";
-    menu_about.style.display = "none";
-    btn_calculate.style.display = "block";
-})
-
-nav_weapon.addEventListener("click", function () {
-    toggleNavbar();
-    menu_construct.style.display = "none";
-    menu_weapon.style.display = "block";
-    menu_cub.style.display = "none";
+    menu_serum.style.display = "none";
     menu_about.style.display = "none";
     btn_calculate.style.display = "block";
 })
@@ -39,17 +30,28 @@ nav_weapon.addEventListener("click", function () {
 nav_cub.addEventListener("click", function () {
     toggleNavbar();
     menu_construct.style.display = "none";
-    menu_weapon.style.display = "none";
     menu_cub.style.display = "block";
+    menu_serum.style.display = "none";
+    menu_about.style.display = "none";
+    btn_calculate.style.display = "block";
+})
+
+nav_serum.addEventListener("click", function () {
+    toggleNavbar();
+    menu_construct.style.display = "none";
+    menu_cub.style.display = "none";
+    menu_serum.style.display = "block";
     menu_about.style.display = "none";
     btn_calculate.style.display = "block";
 })
 
 nav_about.addEventListener("click", function () {
-    toggleNavbar();
+    if(hamb_menu.classList.contains("open") && hamb_icon.classList.contains("open")){
+        toggleNavbar();
+    }
     menu_construct.style.display = "none";
-    menu_weapon.style.display = "none";
     menu_cub.style.display = "none";
+    menu_serum.style.display = "none";
     menu_about.style.display = "block";
     btn_calculate.style.display = "none";
     menu_result.style.display = "none";
--- js/result.js
+++ js/result.js
@@ -1,6 +1,11 @@
 const lvl_exp = [];
 var exp = 0;
 var exp_needed = 0;
+var cogs_needed = 0;
+var sp_needed = 0;
+const exp_pod_S = 100;
+const exp_pod_M = 1000;
+const exp_pod_L = 3000;
 
 for (let i = 0; i < 80; i++) {
     lvl_exp.push(exp);
@@ -13,14 +18,18 @@ for (let i = 0; i < 80; i++) {
     else if (i < 80) exp += 1200;
 }
 
-cogs_needed = 0;
 const rank_cogs = [
     0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 42500, 50000, 60000, 70000, 80000, 100000
 ];
 
-const exp_pod_S = 100
-const exp_pod_M = 1000
-const exp_pod_L = 3000
+// skill value 1 artinya skill_cogs[1] dan skill_point[1]
+// skill berjumlah 8, untuk skill 9 adalah leader dgn cogs = 25k dan sp = 3
+const skill_cogs = [
+    0, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 12000, 14000, 16000, 18000, 20000, 22000, 24000
+];
+const skill_point = [
+    0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 5, 5
+];
 
 function round_up(pembilang, penyebut) {
     if (pembilang / penyebut > Math.floor(pembilang / penyebut)){
@@ -30,30 +39,29 @@ function round_up(pembilang, penyebut) {
     }
 }
 
-// event shop
-// stage_serum = 30
-// stage_drop = 82
-// shop_exp_pod = 125 # exp pod L x6 = 18000 exp
-
 function calculate() {
     var lvl_start = parseInt(document.querySelector('#lvl_input').value);
     var rankval = parseInt(document.querySelector('#rank_option').value);
+    // skill id 0 tidak ada maka diisi dgn 0
+    var skill_id_val = [0];
+    for (let i = 1; i <= 8; i++) {
+        var name = '#skill_' + i;
+        skill_id_val[i] = parseInt(document.querySelector(name).value);
+    }
+    var skill_id_9 = document.querySelector('#skill_9');
+    // ================= result view =========================
     var result_view = document.getElementById("result");
     var exp_pod_view = document.getElementById("exp_pod");
+    var skill_point_view = document.getElementById("skill_point");
     var total_cogs_view = document.getElementById("rank_cogs");
 
     result_view.style.display = "block";
 
+    // ===============  Construct Level ====================
     for (let i = lvl_start; i < lvl_exp.length; i++) {
         exp_needed += lvl_exp[i];
     }
-    
-    for (let i = rankval; i < rank_cogs.length; i++) {
-        cogs_needed += rank_cogs[i];
-    }
-
     var qty_exp_pod_L = round_up(exp_needed, exp_pod_L);
-
     if (lvl_start == 80) {
         exp_pod_view.style.display = "none";
     } else {
@@ -61,6 +69,16 @@ function calculate() {
         document.querySelector('#exp_pod_item').textContent = qty_exp_pod_L;
     }
 
+    // ================= Construct Rank =====================
+    for (let i = rankval; i < rank_cogs.length; i++) {
+        cogs_needed += rank_cogs[i];
+    }
+
+    // ============= Construct skill point =====================
+    // if total skill point == 0 display = none
+
+
+    // =================== total cogs =========================
     if (cogs_needed == 0) {
         total_cogs_view.style.display = "none";
     } else {
@@ -71,3 +89,86 @@ function calculate() {
     exp_needed = 0;
     cogs_needed = 0;
 }
+
+// ===========================input number ======================== //
+
+function imposeMinMax(el) {
+    if (el.value != '') {
+        if (parseInt(el.value) < parseInt(el.min)) {
+            el.value = el.min;
+        }
+        if (parseInt(el.value) > parseInt(el.max)) {
+            el.value = el.max;
+        }
+    } else {
+        el.value = el.min;
+    }
+}
+
+var index = 0;
+var interval;
+var timeout;
+var stopFlag=false;
+
+function clearAll(){
+    clearTimeout(timeout);
+    clearInterval(interval);
+}
+
+function modIn(el) {
+    var inId = el.id;
+    if (inId.charAt(0) == 'p'){
+        var targetId = 'skill_' + inId.charAt(2);
+        // console.log("+"+targetId);
+        var maxValue = parseInt(document.getElementById(targetId).max);
+        var actValue = parseInt(document.getElementById(targetId).value);
+        index = actValue;
+        if (actValue < maxValue){
+            stopFlag=false;
+            document.getElementById(targetId).value++;
+        }else {
+            stopFlag=true;
+        }
+        timeout = setTimeout(function(){
+            interval = setInterval(function(){        
+                if(index+1 >= maxValue){
+                    index=0;
+                    stopFlag=true;
+                }  
+                if(stopFlag==false){
+                    document.getElementById(targetId).value++;
+                } 
+                index++;
+            }, 100);
+        }, 200);      
+        imposeMinMax(document.getElementById(targetId));
+    }
+    if (inId.charAt(0) == 'n') {
+        var targetId = 'skill_' + inId.charAt(2);
+        // console.log("-"+targetId);
+        var minValue = parseInt(document.getElementById(targetId).min);
+        var actValue = parseInt(document.getElementById(targetId).value);
+        index = actValue;
+        if(actValue > minValue){
+            stopFlag=false;
+            document.getElementById(targetId).value--;
+        }else{
+            stopFlag=true;
+        }
+        timeout = setTimeout(function(){
+            interval = setInterval(function(){        
+                if(index-1 <= minValue){
+                    index=0;
+                    stopFlag=true;
+                }  
+                if(stopFlag==false){
+                    document.getElementById(targetId).value--;
+                } 
+                index--;
+            }, 100);
+        }, 200);      
+        imposeMinMax(document.getElementById(targetId));
+    }
+}
+
+// ====================================================================== //

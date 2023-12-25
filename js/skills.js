96fb822ccafd6cff14bf6459f0323480f60ee740
mcgalih <mcgalihramdhan@gmail.com>
2023-12-25 11:14:38 +0700

    add custom skills button

--- js/skills.js
+++ js/skills.js
@@ -1,31 +1,37 @@
 const max_skill = document.getElementById("max-skill");
 const min_skill = document.getElementById("min-skill");
+const custom_skill = document.getElementById("custom-skill");
 
 max_skill.addEventListener("click", function () {
     min_skill.classList.remove("minmax-toggle");
     max_skill.classList.add("minmax-toggle");
+    custom_skill.classList.remove("minmax-toggle");
     for (let i = 1; i <= 8; i++) {
         document.getElementById("skill_" + i).value = parseInt(18);
     }
     document.getElementById("skill_9").checked = true;
+    document.getElementById("skills-wrapper").style.display = "none";
 })
 
 min_skill.addEventListener("click", function () {
     min_skill.classList.add("minmax-toggle");
     max_skill.classList.remove("minmax-toggle");
+    custom_skill.classList.remove("minmax-toggle");
     for (let i = 1; i <= 8; i++) {
         document.getElementById("skill_" + i).value = parseInt(1);
     }
     document.getElementById("skill_9").checked = false;
+    document.getElementById("skills-wrapper").style.display = "none";
 })
 
-function resetminmax() {
+custom_skill.addEventListener("click", function(){
     min_skill.classList.remove("minmax-toggle");
     max_skill.classList.remove("minmax-toggle");
-}
+    custom_skill.classList.add("minmax-toggle");
+    document.getElementById("skills-wrapper").style.display = "grid";
+})
 
 function imposeMinMax(el) {
-    resetminmax();
     if (el.value != '') {
         if (parseInt(el.value) < parseInt(el.min)) {
             el.value = el.min;

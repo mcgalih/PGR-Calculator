d5a402e26409c4fa3eab327651430b27c7e1fc67
mcgalih <mcgalihramdhan@gmail.com>
2023-12-27 22:02:18 +0700

    memory overclock button

--- js/memory.js
+++ js/memory.js
@@ -1,18 +1,65 @@
-var mem_overclock_img = document.getElementsByClassName("mem-overclock-img");
-var mem_1_value = document.getElementsByName("m1");
-var next_overclock = document.getElementById("btn-mem-overclock");
+const max_memory = document.getElementById("max-memory");
+const min_memory = document.getElementById("min-memory");
+const custom_memory = document.getElementById("custom-memory");
 
-for (let i = 1; i < mem_overclock_img.length; i++) {
-    mem_overclock_img[i].style.display = "none";
+// for (let i = 1; i <= 6; i++) {
+//     // initial memory stamp display, overclock 0 -> display = block
+//     for (let j = 1; j <= 4; j++) {
+//         m1_overclock_img[j].style.display = "none";
+//         document.getElementsByClassName("m"+i+"-overclock-img")[j].style.display = "none";
+//     }
+// }
+// initial memory stamp display, overclock 0 -> display = block
+for (let j = 1; j < 5; j++) {
+    document.getElementsByClassName("m1-overclock-img")[j].style.display = "none";
 }
 
-let mem_iterator = 0;
-next_overclock.addEventListener("click", () => {
-    mem_iterator++;
-    if (mem_iterator > 4) mem_iterator = 0;
-    mem_1_value[mem_iterator].checked = true;
-    for (let i = 0; i < mem_overclock_img.length; i++) {
-        mem_overclock_img[i].style.display = "none";
-    }
-    mem_overclock_img[mem_iterator].style.display = "block";
+max_memory.addEventListener("click", function () {
+    min_memory.classList.remove("minmax-toggle");
+    max_memory.classList.add("minmax-toggle");
+    custom_memory.classList.remove("minmax-toggle");
+    //=== reset value
+
+    //===
+    // document.getElementById("memory-wrapper").style.display = "none";
 })
+
+min_memory.addEventListener("click", function () {
+    min_memory.classList.add("minmax-toggle");
+    max_memory.classList.remove("minmax-toggle");
+    custom_memory.classList.remove("minmax-toggle");
+    //=== reset value
+
+    //===
+    // document.getElementById("memory-wrapper").style.display = "none";
+})
+
+custom_memory.addEventListener("click", function(){
+    min_memory.classList.remove("minmax-toggle");
+    max_memory.classList.remove("minmax-toggle");
+    custom_memory.classList.add("minmax-toggle");
+    //=== reset value
+
+    //===
+    // document.getElementById("memory-wrapper").style.display = "grid";
+})
+
+var m_overclock = 0;
+function mod_m_overclock(el){
+    id = parseInt(el.id.charAt(1));
+    var val = document.getElementsByName("m" + id);
+    var img = document.getElementsByClassName("m"+id+"-overclock-img");
+    for (let i = 0; i <= 4; i++) {
+        if(val[i].checked){
+            m_overclock = parseInt(val[i].value);
+        }
+    }
+    m_overclock++;
+    if (m_overclock > 4) m_overclock = 0;
+    val[m_overclock].checked = true;
+
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+    }
+    img[m_overclock].style.display = "block";
+}
--- js/result.js
+++ js/result.js
@@ -41,23 +41,38 @@ function round_up(pembilang, penyebut) {
 }
 
 function calculate() {
+    //====== construct lvl value
     var lvl_start = parseInt(document.querySelector('#lvl_input').value);
+    //====== rank value
     var rankval = parseInt(document.querySelector('#rank_option').value);
-    // skill id 0 tidak ada maka diisi dgn 0
-    var skill_id_val = [0];
+    //====== skill 1~8 value
+    var skill_id_val = [0]; // skill id 0 tidak ada maka diisi dgn 0
     for (let i = 1; i <= 8; i++) {
         var name = '#skill_' + i;
         skill_id_val[i] = parseInt(document.querySelector(name).value);
     }
     // console.table(skill_id_val);
-    // ================= result view =========================
+
+    //====== memory overclock 1-6 value
+    var m_id_overclock_val = [0]; // memory id 0 tidak ada maka diisi dgn 0
+    for (let i = 1; i <= 6; i++) {
+        for (let j = 0; j <= 4; j++) {
+            var m_overclock_val = document.getElementsByName("m"+i);
+            if(m_overclock_val[j].checked){
+                console.log(parseInt(m_overclock_val[j].value));
+                m_id_overclock_val[i] = parseInt(m_overclock_val[j].value);
+            }
+        }
+    }
+    // console.log(m_id_overclock_val);
+    // ============================== result view ==============================
     var result_view = document.getElementById("result");
     var exp_pod_view = document.getElementById("exp_pod");
     var skill_point_view = document.getElementById("skill_point");
     var total_cogs_view = document.getElementById("rank_cogs");
     result_view.style.display = "block";
 
-    // ===============  Construct Level ====================
+    // ============================ Construct Level ============================
     for (let i = lvl_start; i < lvl_exp.length; i++) {
         exp_needed += lvl_exp[i];
     }
@@ -69,12 +84,12 @@ function calculate() {
         document.querySelector('#exp_pod_item').textContent = qty_exp_pod_L;
     }
 
-    // ================= Construct Rank ========================
+    // ============================ Construct Rank =============================
     for (let i = rankval; i < rank_cogs.length; i++) {
         cogs_needed += rank_cogs[i];
     }
 
-    // ============= Construct skill point =====================
+    // ========================= Construct skill point =========================
     for (let i = 1; i <= skill_id_val.length; i++) {
         for (let j = skill_id_val[i]; j < skill_cogs.length; j++) {
             cogs_needed += skill_cogs[j];
@@ -98,7 +113,7 @@ function calculate() {
         document.querySelector('#skill_point_item').textContent = sp_needed;
     }
 
-    // =================== total cogs =========================
+    // ============================== total cogs ================================
     if (cogs_needed == 0) {
         total_cogs_view.style.display = "none";
     } else {
